da82981c775361dbee5522936aa1f1ebe79d7345
phoenix377 <phoenix.number377@gmail.com>
2019-07-08 06:04:15 +0800

    change subscription assets remove unsed code fix replacable product image error

--- src/assets/custom.js
+++ src/assets/custom.js
@@ -1,35 +1,3 @@
-/**
- * Include your custom JavaScript here.
- *
- * We also offer some hooks so you can plug your own logic. For instance, if you want to be notified when the variant
- * changes on product page, you can attach a listener to the document:
- *
- * document.addEventListener('variant:changed', function(event) {
- *   var variant = event.detail.variant; // Gives you access to the whole variant details
- * });
- *
- * You can also add a listener whenever a product is added to the cart:
- *
- * document.addEventListener('product:added', function(event) {
- *   var variant = event.detail.variant; // Get the variant that was added
- *   var quantity = event.detail.quantity; // Get the quantity that was added
- * });
- */
- 
-
-
-// function toggleAriaHiddenRole(boolean) {
-//   if (slide.getAttribute('aria-hidden') === "true") {
-//     changeAriaHiddenRole(slide, false)
-//   } else {
-//     changeAriaHiddenRole(slide, true)
-//   }
-// }
-
-
-
-
-
 var filterBar = document.querySelector('.filter-bar--footer')
 
 window.mobilecheck = function() {
@@ -58,7 +26,7 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
 
  // Testimonial Slider Start
  
-   	jQuery('.single-item2').slick({
+   	/* jQuery('.single-item2').slick({
 		infinite: true,
 		speed: 300,
 		autoplay: true,
@@ -101,6 +69,6 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
 			// instead of a settings object
 		]				
 	
-	}); 
+	});  */
   // Testimonial Slider End
   
